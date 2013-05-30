4463c6efe35725a0aabdb310fdf0c4be23043456
Joe Yates <joe.g.yates@gmail.com>
2013-05-30 19:20:48 +0200

    FactoryGirl dies if we include validations.
    We get a 'stack level too deep' error during saving of factories.

--- lib/round_trip/project.rb
+++ lib/round_trip/project.rb
@@ -1,8 +1,5 @@
 require 'active_record'
 
 class RoundTrip::Project < ActiveRecord::Base
-  include ActiveRecord::Validations
-
-  validates_presence_of :name
 end
 
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,5 @@
 require 'model_spec_helper'
 
 describe RoundTrip::Project do
-  it { should validate_presence_of(:name) }
 end
 
