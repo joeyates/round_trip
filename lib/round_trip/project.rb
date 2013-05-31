a1869234f7a319997a49acd233b9e0b26750512e
Joe Yates <joe.g.yates@gmail.com>
2013-05-31 14:57:49 +0200

    Validations now work again with FactoryGirl

--- lib/round_trip/project.rb
+++ lib/round_trip/project.rb
@@ -1,6 +1,10 @@
 require 'active_record'
 
 class RoundTrip::Project < ActiveRecord::Base
+  include ActiveRecord::Validations
+
+  validates_presence_of :name
+
   serialize :redmine
   serialize :trello
 
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,6 @@
 require 'model_spec_helper'
 
 describe RoundTrip::Project do
+  it { should validate_presence_of(:name) }
 end
 
