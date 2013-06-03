33c306fb37f73344e07cf4b291adff1c6d06641d
Joe Yates <joe.g.yates@gmail.com>
2013-06-04 01:19:00 +0200

    Use factories

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -5,7 +5,18 @@ FactoryGirl.define do
       sequence(:redmine_project_id) { |id| id }
       sequence(:redmine_subject) { |n| "Subject #{n}" }
       sequence(:redmine_description) { |n| "Description #{n}" }
-      redmine_updated_on = DateTime.now
+      redmine_updated_on DateTime.now.strftime("%Y/%m/%d %H:%M:%S %Z")
+    end
+
+    factory :trello_ticket do
+      sequence(:trello_id) { |id| id }
+      sequence(:trello_board_id) { |id| id }
+      sequence(:trello_list_id) { |id| id }
+      sequence(:trello_name) { |n| "Name #{n}" }
+      sequence(:trello_desc) { |n| "Description #{n}" }
+      trello_last_activity_date DateTime.now.strftime("%Y/%m/%d %H:%M:%S %Z")
+      sequence(:trello_url) { |n| "http://example.com/card/#{n}" }
+      trello_closed false
     end
   end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -11,31 +11,21 @@ describe RoundTrip::Ticket do
     end
 
     describe '.create_from_redmine_resource' do
-      let(:redmine_project_id) { 12345 }
       let(:trello_card_id) { 'abcdef' }
-      let(:issue_description) { "The description\n" }
       let(:issue_description_with_trello_id) { "## Trello card id: #{trello_card_id} ##\nThe description\n" }
       let(:resource_attribs) do
         {
-          :id => 123,
-          :project => stub('RoundTrip::Redmine::Issue', :id => redmine_project_id),
-          :updated_on => '2013/05/17 18:15:28 +0200',
-          :subject => 'The Subject',
-          :description => issue_description,
+          :id => ticket_attributes[:redmine_id],
+          :project => stub('RoundTrip::Redmine::Issue', :id => ticket_attributes[:redmine_project_id]),
+          :updated_on => ticket_attributes[:redmine_updated_on],
+          :subject => ticket_attributes[:redmine_subject],
+          :description => ticket_attributes[:redmine_description],
         }
       end
       let(:resource_attribs_with_trello_id) { resource_attribs.merge(:description => issue_description_with_trello_id) }
       let(:issue_resource) { stub('RoundTrip::Redmine::Issue', resource_attribs) }
       let(:issue_resource_with_trello_id) { stub('RoundTrip::Redmine::Issue', resource_attribs_with_trello_id) }
-      let(:ticket_attributes) do
-        {
-          :redmine_id => 123,
-          :redmine_project_id => redmine_project_id,
-          :redmine_updated_on => '2013/05/17 18:15:28 +0200',
-          :redmine_subject => 'The Subject',
-          :redmine_description => issue_description,
-        }
-      end
+      let(:ticket_attributes) { attributes_for(:redmine_ticket) }
 
       it 'creates tickets' do
         RoundTrip::Ticket.create_from_redmine_resource(issue_resource)
@@ -56,40 +46,22 @@ describe RoundTrip::Ticket do
     end
 
     describe '.create_from_trello_card' do
-      let(:card_id) { 'aaaa' }
-      let(:board_id) { 'bbbb' }
-      let(:list_id) { 'cccc' }
-      let(:redmine_issue_id) { 12345 }
-      let(:card_name) { 'Card name' }
-      let(:card_description) { "Card description\n" }
-      let(:card_description_with_redmine_id) { "## Redmine issue id: #{redmine_issue_id} ##\n#{card_description}" }
-      let(:card_last_activity_date) { '2013-06-02T18:30:05+02:00' }
-      let(:card_url) { 'http://example.com/card/1' }
+      let(:ticket_attributes) { attributes_for(:trello_ticket) }
       let(:card_attributes) do
         {
-          :id => card_id,
-          :board_id => board_id,
-          :list_id => list_id,
-          :name => card_name,
-          :description => card_description,
-          :last_activity_date => card_last_activity_date,
-          :url => card_url,
-          :closed => false,
+          :id => ticket_attributes[:trello_id],
+          :board_id => ticket_attributes[:trello_board_id],
+          :list_id => ticket_attributes[:trello_list_id],
+          :name => ticket_attributes[:trello_name],
+          :description => ticket_attributes[:trello_desc],
+          :last_activity_date => ticket_attributes[:trello_last_activity_date],
+          :url => ticket_attributes[:trello_url],
+          :closed => ticket_attributes[:trello_closed],
         }
       end
+      let(:redmine_issue_id) { 12345 }
+      let(:card_description_with_redmine_id) { "## Redmine issue id: #{redmine_issue_id} ##\n#{card_attributes[:description]}" }
       let(:trello_card) { stub('Trello::Card', card_attributes) }
-      let(:ticket_attributes) do
-        {
-          :trello_id => card_id,
-          :trello_board_id => board_id,
-          :trello_list_id => list_id,
-          :trello_name => card_name,
-          :trello_desc => card_description,
-          :trello_last_activity_date => card_last_activity_date,
-          :trello_url => card_url,
-          :trello_closed => false,
-        }
-      end
       let(:ticket_attributes_with_redmine_id) do
         ticket_attributes.merge(
           :trello_desc => card_description_with_redmine_id,
