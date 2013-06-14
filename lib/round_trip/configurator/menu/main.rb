590533c27e8233c317cf19c31f285356d80c7b9a
Joe Yates <joe.g.yates@gmail.com>
2013-06-14 12:02:16 +0200

    Extract methods to reduce indentation

--- lib/round_trip/configurator/menu/main.rb
+++ lib/round_trip/configurator/menu/main.rb
@@ -7,27 +7,34 @@ module RoundTrip
     def run
       loop do
         system('clear')
-        high_line.choose do |menu|
-          menu.header    = 'Choose a project'
-          menu.flow      = :columns_down
-          menu.prompt    = 'What now> '
-          menu.select_by = :index_or_name
-          Project.all.each do |project|
-            menu.choice(project.name) do
-              Configurator::Menu::Project.new(high_line).run(Configurator::Presenter::Project.new(project))
-            end
-          end
-          menu.choice('add a project') do
-            name = high_line.ask('name: ')
-            if name.match(/^[\w\s\d]+$/)
-              Project.create!(:name => name)
-            end
-          end
-          menu.choice('quit (q)') do
-            return
+        done = show_menu
+        return if done
+      end
+    end
+
+    def show_menu
+      high_line.choose do |menu|
+        menu.header    = 'Choose a project'
+        menu.flow      = :columns_down
+        menu.prompt    = 'What now> '
+        menu.select_by = :index_or_name
+        Project.all.each do |project|
+          menu.choice(project.name) do
+            project_presenter = Configurator::Presenter::Project.new(project)
+            Configurator::Menu::Project.new(high_line, project_presenter).run
           end
         end
+        menu.choice('add a project') do
+          name = high_line.ask('name: ')
+          if name.match(/^[\w\s\d]+$/)
+            Project.create!(:name => name)
+          end
+        end
+        menu.choice('quit (q)') do
+          return true
+        end
       end
+      false
     end
   end
 end
--- lib/round_trip/configurator/menu/project.rb
+++ lib/round_trip/configurator/menu/project.rb
@@ -4,50 +4,63 @@ require 'round_trip/trello/connection_tester'
 
 module RoundTrip
   class Configurator::Menu::Project < Configurator::Menu::Base
-    def run(project)
+    attr_reader :project
+
+    def initialize(high_line, project)
+      super(high_line)
+      @project = project
+    end
+
+    def run
       loop do
         system('clear')
-        high_line.choose do |menu|
-          menu.header    = project.to_s + "\n\nEdit a setting"
-          menu.flow      = :columns_down
-          menu.prompt    = 'What now> '
-          menu.select_by = :index_or_name
-          menu.choice('name') do
-            project.name = high_line.ask("name: ") do |q|
-              q.default = project.name if project.name and project.name != ''
+        done = show_menu
+        return if done
+      end
+    end
+
+    def show_menu
+      high_line.choose do |menu|
+        menu.header    = project.to_s + "\n\nEdit a setting"
+        menu.flow      = :columns_down
+        menu.prompt    = 'What now> '
+        menu.select_by = :index_or_name
+        menu.choice('name') do
+          project.name = high_line.ask("name: ") do |q|
+            q.default = project.name if project.name and project.name != ''
+          end
+        end
+        project.each_configuration do |key, human_name, value|
+          menu.choice(human_name) do
+            c = project.config.clone
+            c[key] = high_line.ask("#{human_name}: ") do |q|
+              q.default = c[key] if c[key]
             end
+            project.config = c
           end
-          project.each_configuration do |key, human_name, value|
-            menu.choice(human_name) do
-              c = project.config.clone
-              c[key] = high_line.ask("#{human_name}: ") do |q|
-                q.default = c[key] if c[key]
-              end
-              project.config = c
-            end
-          end
-          menu.choice('test redmine connection') do
-            result, message = Redmine::ConnectionTester.new(project).run
-            high_line.ask "#{message}\nPress a key... "
-          end
-          menu.choice('test trello connection') do
-            result, message = Trello::ConnectionTester.new(project).run
-            high_line.ask "#{message}\nPress a key... "
-          end
-          menu.choice('save') do
-            project.save!
-            return
-          end
-          menu.choice('quit (q)') do
-            if project.changed?
-              confirmed = high_line.agree("The project has been modified.\nExit without saving? ")
-              return if confirmed
-            else
-              return
-            end
+        end
+        menu.choice('test redmine connection') do
+          result, message = Redmine::ConnectionTester.new(project).run
+          high_line.ask "#{message}\nPress a key... "
+        end
+        menu.choice('test trello connection') do
+          result, message = Trello::ConnectionTester.new(project).run
+          high_line.ask "#{message}\nPress a key... "
+        end
+        menu.choice('save') do
+          project.save!
+          return true
+        end
+        menu.choice('quit (q)') do
+          if project.changed?
+            confirmed = high_line.agree("The project has been modified.\nExit without saving? ")
+            return true if confirmed
+          else
+            return true
           end
         end
       end
+      false
     end
   end
 end
