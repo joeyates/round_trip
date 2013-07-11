47a0adf35ae607fc570cb67dd00310046de3ab91
Joe Yates <joe.g.yates@gmail.com>
2013-07-11 12:10:30 +0200

    Avoid deprecation warning
    Warning:
    Accessing shared_examples defined across contexts is deprecated.
    Please declare shared_examples within a shared context,
    or at the top level.

--- spec/support/models/shared_examples_for_models.rb
+++ spec/support/models/shared_examples_for_models.rb
@@ -1,28 +1,26 @@
-module SharedExamplesForModels
-  shared_examples_for 'a class with constructor arity' do |count|
-    if count > 0
-      it "raises an error unless #{count} #{count == 1? 'parameter is' : 'parameters are'} supplied" do
-        expect {
-          described_class.new
-        }.to raise_error(ArgumentError, "wrong number of arguments (0 for #{count})")
-      end
-    else
-      it "raises an error parameters are supplied" do
-        expect {
-          described_class.new('foo')
-        }.to raise_error(ArgumentError, "wrong number of arguments(1 for 0)")
-      end
+shared_examples_for 'a class with constructor arity' do |count|
+  if count > 0
+    it "raises an error unless #{count} #{count == 1? 'parameter is' : 'parameters are'} supplied" do
+      expect {
+        described_class.new
+      }.to raise_error(ArgumentError, "wrong number of arguments (0 for #{count})")
+    end
+  else
+    it "raises an error parameters are supplied" do
+      expect {
+        described_class.new('foo')
+      }.to raise_error(ArgumentError, "wrong number of arguments(1 for 0)")
     end
   end
+end
 
-  shared_context 'ticket project scoping' do
-    let(:project_attributes) { attributes_for(:project) }
-    let(:project) { create(:project, project_attributes) }
-    let(:for_project_scope) { stub('ActiveRecord::Relation Ticket.for_project scope') }
+shared_context 'ticket project scoping' do
+  let(:project_attributes) { attributes_for(:project) }
+  let(:project) { create(:project, project_attributes) }
+  let(:for_project_scope) { stub('ActiveRecord::Relation Ticket.for_project scope') }
 
-    before do
-      RoundTrip::Ticket.stubs(:for_project).with(project.id).returns(for_project_scope)
-    end
+  before do
+    RoundTrip::Ticket.stubs(:for_project).with(project.id).returns(for_project_scope)
   end
 end
 
